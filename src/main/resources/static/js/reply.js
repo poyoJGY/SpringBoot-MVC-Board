345740742b7281ff1e047665a14a67da0e45a0a5
poyoJGY <duke407@naver.com>
2026-01-15 00:32:18 +0900

    Add reply list, reply paging, etc.

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -2,6 +2,20 @@ async function get1(bno) {
 
     const result = await axios.get(`/api/replies/list/${bno}`)
 
-    console.log(result)
+    return result;
 
+}
+
+async function getList({bno, page, size, goLast}) {
+
+    const result = await axios.get(`/api/replies/list/${bno}`, {params: {page, size}})
+
+    if (goLast) {
+        const total = result.data.total
+        const lastPage = parseInt(Math.ceil(total / size))
+
+        return getList({bno:bno, page:lastPage, seize:size})
+    }
+
+    return result.data
 }
