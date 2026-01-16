211b12e79afdf2c227ae20641218a1da9c623efe
poyoJGY <duke407@naver.com>
2026-01-17 00:04:51 +0900

    Add file upload GET, POST, DELETE, Thumbnailator library

---
+++ src/main/java/me/jgy/b01/dto/upload/UploadFileDTO.java
@@ -0,0 +1,12 @@
+package me.jgy.b01.dto.upload;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Data
+public class UploadFileDTO {
+
+    private List<MultipartFile> files;
+}
