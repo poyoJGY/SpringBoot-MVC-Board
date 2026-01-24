77afc8a081d5ea9c9ebadd6a70109b9709b6076a
poyoJGY <duke407@naver.com>
2026-01-24 19:40:16 +0900

    게시물 첨부파일 삭제 기능 구현

--- src/main/java/me/jgy/b01/controller/BoardController.java
+++ src/main/java/me/jgy/b01/controller/BoardController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import me.jgy.b01.dto.*;
 import me.jgy.b01.service.BoardService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,12 +16,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
 @Controller
 @RequestMapping("/board")
 @Log4j2
 @RequiredArgsConstructor
 public class BoardController {
 
+    @Value("${me.jgy.upload.path}")
+    private String uploadPath;
     private final BoardService boardService;
 
     @Operation(summary = "list")
@@ -104,14 +113,45 @@ public class BoardController {
 
     @Operation(summary = "remove")
     @PostMapping("/remove")
-    public String remove(Long bno, RedirectAttributes redirectAttributes) {
+    public String remove(BoardDTO boardDTO, RedirectAttributes redirectAttributes) {
 
+        Long bno = boardDTO.getBno();
         log.info("remove post.. " + bno);
 
         boardService.remove(bno);
 
+        // 게시물이 데이터베이스상에서 삭제 되었다면 첨부 파일 삭제
+        log.info(boardDTO.getFileNames());
+        List<String> fileNames = boardDTO.getFileNames();
+
+        if (fileNames != null && fileNames.size() > 0) {
+            removeFiles(fileNames);
+        }
+
         redirectAttributes.addFlashAttribute("result", "removed");
 
         return "redirect:/board/list";
+    }
+
+    private void removeFiles(List<String> files) {
+
+        for (String fileName : files) {
+            FileSystemResource resource = new FileSystemResource(uploadPath + File.separator + fileName);
+            String resourceName = resource.getFilename();
+
+            try {
+                String contentType = Files.probeContentType(resource.getFile().toPath());
+                resource.getFile().delete();
+
+                // 섬네일이 좋재한다면
+                if (contentType.startsWith("image")) {
+                    File thumbnailFile = new File(uploadPath + File.separator + "s_" + fileName);
+
+                    thumbnailFile.delete();
+                }
+            } catch (IOException e) {
+                log.error(e.getMessage());
+            }
+        }
     }
 }
