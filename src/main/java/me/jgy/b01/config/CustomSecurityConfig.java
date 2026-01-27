24a6c4c9bcd3451152b54c788a7b27c621e29fa4
poyoJGY <duke407@naver.com>
2026-01-27 23:47:56 +0900

    Authentication, Authorization

--- src/main/java/me/jgy/b01/config/CustomSecurityConfig.java
+++ src/main/java/me/jgy/b01/config/CustomSecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Log4j2
@@ -15,9 +17,17 @@ import org.springframework.security.web.SecurityFilterChain;
 public class CustomSecurityConfig {
 
     @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         log.info("---------------configure---------------");
+
+        http.formLogin(config ->
+                config.loginPage("/member/login"));
 
         return http.build();
     }
